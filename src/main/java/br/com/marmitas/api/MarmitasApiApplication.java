c948dcd40668f615ecc3ed09039f5cf522ef0698
KailaneMV <kvilelaalves@gmail.com>
2026-02-05 13:46:35 -0300

    feat: initial spring boot project

---
+++ src/main/java/br/com/marmitas/api/MarmitasApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.marmitas.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarmitasApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarmitasApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/marmitas/api/MarmitasApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.marmitas.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarmitasApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
